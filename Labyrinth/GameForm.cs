dae06714dc65034765c8c811eac1e33a9fda471b
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-06-14 16:17:57 +0300

    Add project files.

---
+++ Labyrinth/GameForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labyrinth
+{
+    public partial class GameForm : Form
+    {
+        private readonly int CellSize;
+        private readonly int MazeSize;
+        private int playerX = 1;
+        private int playerY = 0;
+        private int[,] array2;
+        public GameForm(int[,] array2)
+        {
+            InitializeComponent();
+            this.array2 = array2;
+            CellSize = this.array2.GetLength(0) * 2;
+            MazeSize = this.array2.GetLength(1);
+            InitializeMaze();
+        }
+
+        private void InitializeMaze()
+        {
+            this.ClientSize = new Size(MazeSize * CellSize, MazeSize * CellSize);
+            this.Text = "Maze Game";
+        }
+
+        private void GameForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < array2.GetLength(0); i++)
+            {
+                for(int j = 0; j < array2.GetLength(1); j++)
+                {
+                    Brush brush = array2[i, j] == 1? Brushes.Black : Brushes.White;
+                    g.FillRectangle(brush, j * CellSize, i * CellSize, CellSize, CellSize);
+                }
+            }
+            g.FillRectangle(Brushes.Red, playerX * CellSize, playerY * CellSize, CellSize, CellSize);
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            int newX = playerX;
+            int newY = playerY;
+
+            switch(e.KeyCode) 
+            {
+                case Keys.Up: { newY = Math.Max(0, playerY - 1); break; }
+                case Keys.Down: { newY = Math.Min(MazeSize - 1, playerY + 1); break; }
+                case Keys.Left: { newY = Math.Max(0, playerX - 1); break; }
+                case Keys.Right: { newX = Math.Min(MazeSize - 1, playerX + 1); break; }
+            }
+            if (array2[newX, newY] == 0)
+            {
+                playerX = newX;
+                playerY = newY;
+                Invalidate();
+            }    
+            if(playerX==array2.GetLength(1)-1 || playerY==array2.GetLength(0)-1) 
+            {
+                MessageBox.Show("Game over");
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Labyrinth/LoadMapForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labyrinth
+{
+    public partial class LoadMapForm : Form
+    {
+        private int[,] array2;
+
+        public LoadMapForm()
+        {
+            (Height, Width) = (400, 400);
+            (FormBorderStyle, MaximizeBox) = (FormBorderStyle.FixedDialog, false);
+            AddControls();
+            InitializeComponent();
+        }
+
+        private void AddControls()
+        {
+            Label label = new Label
+            {
+                Text = "Choose a map",
+                Font = new Font("Vernada", 12),
+                AutoSize = true
+            };
+            Controls.Add(label);
+            CenterControl(label);
+
+            Button loadMap = new Button()
+            {
+                Text = "Browse",
+                Font = new Font("Vernada", 12),
+                AutoSize = true
+            };
+            Controls.Add(loadMap);
+            CenterControl(loadMap, label);
+
+            Label labelInfo = new Label()
+            {
+                Font = new Font("Vernada", 12),
+                AutoSize = true
+            };
+            Controls.Add(labelInfo);
+            CenterControl(labelInfo, loadMap);
+
+            Button startButton = new Button()
+            {
+                Enabled = false,
+                AutoSize = true,
+                Font = new Font("Vernada", 12),
+                Text = "Start game"
+            };
+            Controls.Add(startButton);
+            CenterControl(startButton, labelInfo, 5);
+
+            loadMap.Click += (sender, e) =>
+            {
+                OpenFileDialog dialog = new OpenFileDialog()
+                {
+                    Filter = "txt files (*.txt)|*txt"
+                };
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string[] lines = File.ReadAllLines(dialog.FileName);
+                        array2 = new int[lines.Length, lines[0].Split(',').Length];
+                        for (int i = 0; i < lines.Length; i++)
+                        {
+                            string[] values = lines[i].Split(',');
+                            for (int j = 0; j < values.Length; j++)
+                            {
+                                array2[i, j] = int.Parse(values[j].Trim());
+                            }
+                        }
+                        labelInfo.Text = $"Map name: {Path.GetFileName(dialog.FileName)}";
+                        CenterControl(labelInfo, loadMap);
+                        startButton.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }                    
+                }
+            };
+            startButton.Click += (sender, e) =>
+            {
+                GameForm gameForm = new GameForm(array2);
+                gameForm.ShowDialog();
+            };
+            
+        }
+       private void CenterControl(Control? currentControl, Control? lastControl=null, int koeff = 1)
+        {
+            int centerX = (this.ClientSize.Width - currentControl.Width) / 2;
+            int centerY = lastControl != null ? lastControl.Bottom : currentControl.Height;
+
+            if(lastControl != null )
+            {
+                currentControl.Location = new Point(centerX, centerY + lastControl.Height + koeff);
+            }
+            else
+            {
+                currentControl.Location = new Point(centerX, centerY);
+            }
+        }
+        
+    }
+}
